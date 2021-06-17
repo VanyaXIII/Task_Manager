4fd3d9c82e10e7393c587e3cfd50c196a9835112
Ivan <guryanoviv@gmail.com>
2021-06-17 11:24:51 +0300

    InitialCommit#2

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -1,20 +1,15 @@
 package com.example.taskmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.ListView
-import com.example.taskmanager.tasks.Task
+import androidx.appcompat.app.AppCompatActivity
 import com.example.taskmanager.tasks.TaskList
-import com.example.taskmanager.utils.ExecutionPeriod
 import com.example.taskmanager.utils.TasksFileManager
 import com.example.taskmanager.utils.tasksToStrings
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,15 +17,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val taskList = TaskList(TasksFileManager(null, this).load())
         val calendar : CalendarView = findViewById(R.id.calendarView)
-        val list : ListView = findViewById(R.id._dynamic)
+        val list : ListView = findViewById(R.id.listView)
         var tasks: ArrayList<String>
         var c : Calendar = Calendar.getInstance()
         calendar.setOnDateChangeListener { _, year, month, dayOfMonth ->  c = GregorianCalendar(year, month, dayOfMonth)
             tasks = tasksToStrings(taskList.getTaskListByDay(c).tasks)
             list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, tasks)
         }
-        val addingBtn = findViewById<Button>(R.id.addingButton)
-        val executingBtn = findViewById<Button>(R.id.executingButton)
+        val addingBtn = findViewById<Button>(R.id.addingBtn)
+        val executingBtn = findViewById<Button>(R.id.executingBtn)
         addingBtn.setOnClickListener { tasks = tasksToStrings(taskList.getTaskListByDay(c).tasksByDate)
             list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, tasks)}
         executingBtn.setOnClickListener {  tasks = tasksToStrings(taskList.getTaskListByDay(c).tasksByExTime)
