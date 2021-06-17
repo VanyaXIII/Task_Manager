d59b803bbd4d8f974a7642bedb2432b2c5a32bc3
Ivan <guryanoviv@gmail.com>
2021-06-17 11:36:46 +0300

    Initial commit#2

--- app/src/main/java/com/example/taskmanager/utils/ts.kt
+++ app/src/main/java/com/example/taskmanager/utils/ts.kt
@@ -1,6 +1,10 @@
 package com.example.taskmanager.utils
 
+import android.content.Context
 import com.example.taskmanager.tasks.Task
+import com.example.taskmanager.tasks.TaskList
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 
 fun tasksToStrings(tasks:  ArrayList<Task>) : ArrayList<String>{
     if (tasks.size == 0) return ArrayList()
@@ -8,4 +12,10 @@ fun tasksToStrings(tasks:  ArrayList<Task>) : ArrayList<String>{
     for (i in 0 until tasks.size)
         res.add(tasks[i].toString())
     return res
+}
+
+fun initTestingTasks(tasksAsJson : String, context : Context){
+    val mapper = jacksonObjectMapper()
+    val tasks : ArrayList<Task> = mapper.readValue(tasksAsJson)
+    TasksFileManager(TaskList(tasks), context).save()
 }
