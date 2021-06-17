7544ac5dc20288a255cc25ea4f904f16462dfe81
just-4-name <55346973+just-4-name@users.noreply.github.com>
2021-06-17 21:23:29 +0300

    better comparator and sorting of tasks

--- app/src/main/java/com/example/taskmanager/tasks/TaskComparators.kt
+++ app/src/main/java/com/example/taskmanager/tasks/TaskComparators.kt
@@ -1,22 +1,47 @@
 package com.example.taskmanager.tasks
 
+import java.util.*
+import kotlin.Comparator
+
 class TaskComparators {
 
     companion object {
 
         val creatingDateComparator = Comparator<Task> { task1, task2 ->
+            operator fun Task.compareTo(other: Task): Int{
+                val parameters = arrayOf(1,2,5,11,12)
+                for (i in parameters){
+                    if(this.creatingDate[i]>other.creatingDate[i]) return 1
+                    if(this.creatingDate[i]<other.creatingDate[i]) return -1
+                }
+                if(this.isPriority>other.isPriority) return 1
+                if(this.isPriority<other.isPriority) return -1
+                return 0
+            }
             when {
-                task1.creatingDate == task2.creatingDate -> return@Comparator 0
-                task1.creatingDate > task2.creatingDate -> return@Comparator 1
-                else -> return@Comparator -1
+                task1 > task2 -> return@Comparator 1
+                task1 < task2 -> return@Comparator -1
+                else -> return@Comparator 0
             }
         }
 
         val executionPeriodComparator = Comparator<Task> { task1, task2 ->
+            operator fun Task.compareTo(other: Task): Int{
+                val parameters = arrayOf(1,2,5,11,12)
+                for (i in parameters){
+                    if(this.executionPeriod.startDate[i]>other.executionPeriod.startDate[i]) return 1
+                    if(this.executionPeriod.startDate[i]<other.executionPeriod.startDate[i]) return -1
+                }
+                if(this.isPriority>other.isPriority) return 1
+                if(this.isPriority<other.isPriority) return -1
+                return 0
+            }
             when {
-                task1.executionPeriod.startDate == task2.executionPeriod.startDate -> return@Comparator 0
-                task1.executionPeriod.startDate > task2.executionPeriod.startDate -> return@Comparator 1
-                else -> return@Comparator -1
+
+                task1 > task2 -> return@Comparator 1
+                task1 < task2 -> return@Comparator -1
+                else -> return@Comparator 0
+
             }
         }
     }
