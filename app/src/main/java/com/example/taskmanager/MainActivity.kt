36d01ebf729a8e25f57092ed452fd3beb66e0ff6
daniil-vikulov <daniel.vikulov@yandex.ru>
2021-06-18 21:58:17 +0300

    recycle_trash

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -6,11 +6,14 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.taskmanager.notifications.NotificationService
 import com.example.taskmanager.tasks.Task
 import com.example.taskmanager.tasks.TaskList
 import com.example.taskmanager.tasks.TaskManager
 import com.example.taskmanager.tasks.TasksFileHandler
+import com.example.taskmanager.ui.CustomRecyclerAdapter
 import com.example.taskmanager.utils.DatePickerCreator
 import java.util.*
 
@@ -22,22 +25,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val taskList = TaskList(TasksFileHandler(null, this).load())
-        startNotificationService()
+        //startNotificationService()
+        val recyclerView: RecyclerView = findViewById(R.id.rv)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = CustomRecyclerAdapter(fillList(taskList))
         val taskManager = TaskManager(taskList)
         val calendarButton= findViewById<Button>(R.id.calendarButton)
-        val list : ListView = findViewById(R.id.listView)
-        calendarButton.setOnClickListener { DatePickerCreator(this, date, taskManager, list).setDate()}
+        //val list : ListView = findViewById(R.id.listView)
+        //calendarButton.setOnClickListener { DatePickerCreator(this, date, taskManager, list).setDate()}
         var tasks: ArrayList<Task>
         val addingBtn = findViewById<Button>(R.id.addingBtn)
         val executingBtn = findViewById<Button>(R.id.executingBtn)
-        addingBtn.setOnClickListener { tasks = taskManager.getTaskListByDay(date).tasksByDate
-            list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, tasks)}
-        executingBtn.setOnClickListener {  tasks = taskManager.getTaskListByDay(date).tasksByExTime
-            list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, tasks)}
+        /*addingBtn.setOnClickListener { tasks = taskManager.getTaskListByDay(date).tasksByDate
+            list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, tasks)}*/
+        /*executingBtn.setOnClickListener {  tasks = taskManager.getTaskListByDay(date).tasksByExTime
+            list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, tasks)}*/
         val creatingBtn = findViewById<Button>(R.id.createTaskBtn)
         creatingBtn.setOnClickListener {
             startActivity(Intent(this, TaskCreatingActivity::class.java))
         }
+    }
+
+    private fun fillList(taskList: TaskList): List<String> {
+        var tasks: ArrayList<String> = ArrayList()
+            taskList.tasks!!.forEach { tasks.add(it.description) }
+        return tasks
     }
 
     private fun startNotificationService(){
